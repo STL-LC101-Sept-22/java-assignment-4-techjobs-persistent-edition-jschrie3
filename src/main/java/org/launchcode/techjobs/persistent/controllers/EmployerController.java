5cf96e45bc121a30d4c4a2dbbdd2961747254fea
julian <julianschriedel@gmail.com>
2023-03-22 22:00:29 -0500

    finished part 2, tests passing

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,29 +1,39 @@
 package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
+import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("employers")
 public class EmployerController {
 
-    // TODO: Add a private field of EmployerRepository type called employerRepository to EmployerController.
+    // Add a private field of EmployerRepository type called employerRepository to EmployerController.
     //  Give this field an @Autowired annotation.
 
+    @Autowired
+    private EmployerRepository employerRepository;
 
-
-    // TODO: Add an index method that responds at /employers with a list of all employers in the database.
+    // Add an index method that responds at /employers with a list of all employers in the database.
     //  This method should use the template employers/index. To figure out the name of the model attribute
     //  you should use to pass employers into the view, review this template.
 
-
-
+    @GetMapping(value="")
+    public String index(Model model) {
+        List<Employer> employers = (List<Employer>) employerRepository.findAll();
+        model.addAttribute("employers", employers);
+        return "employers/index";
+    }
 
     @GetMapping("add")
     public String displayAddEmployerForm(Model model) {
@@ -35,12 +45,14 @@ public class EmployerController {
     public String processAddEmployerForm(@ModelAttribute @Valid Employer newEmployer,
                                     Errors errors, Model model) {
 
-        // TODO: processAddEmployerForm already takes care of sending the form back if any of the submitted
+        // processAddEmployerForm already takes care of sending the form back if any of the submitted
         //  employer object information is invalid. However, it doesn’t yet contain the code to save a valid
         //  object. Use employerRepository and the appropriate method to do so.
 
         if (errors.hasErrors()) {
             return "employers/add";
+        } else {
+            employerRepository.save(newEmployer);
         }
 
         return "redirect:";
@@ -49,12 +61,12 @@ public class EmployerController {
     @GetMapping("view/{employerId}")
     public String displayViewEmployer(Model model, @PathVariable int employerId) {
 
-        // TODO: displayViewEmployer will be in charge of rendering a page to view the contents of an individual
+        // displayViewEmployer will be in charge of rendering a page to view the contents of an individual
         //  employer object. It will make use of that employer object’s id field to grab the correct information
         //  from employerRepository. optEmployer is currently initialized to null. Replace this using the
         //  .findById() method with the right argument to look for the given employer object from the data layer.
 
-        Optional optEmployer = null;
+        Optional optEmployer = employerRepository.findById(employerId);
         if (optEmployer.isPresent()) {
             Employer employer = (Employer) optEmployer.get();
             model.addAttribute("employer", employer);
--- src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/SkillController.java
@@ -1,10 +1,67 @@
 package org.launchcode.techjobs.persistent.controllers;
 
+import org.launchcode.techjobs.persistent.models.Skill;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("skills")
 public class SkillController {
 
-    // TODO: Create a SkillController class and replicate the steps you followed above for EmployerController.
+    // Create a SkillController class and replicate the steps you followed above for EmployerController.
     //  The new controller should have the methods, index, displayAddSkillForm, processAddSkillForm, and
     //  displayViewSkill. These methods should behave exactly as the corresponding methods in EmployerController.
     //  The relevant templates have already been created for you.
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+    @GetMapping(value="")
+    public String index(Model model) {
+        List<Skill> skills = (List<Skill>) skillRepository.findAll();
+        model.addAttribute("skills", skills);
+        return "skills/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddSkillForm(Model model) {
+        model.addAttribute(new Skill());
+        return "skills/add";
+    }
+
+    @PostMapping("add")
+    public String processAddSkillForm(@ModelAttribute @Valid Skill newSkill,
+                                         Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            return "skills/add";
+        } else {
+            skillRepository.save(newSkill);
+        }
+
+        return "redirect:";
+    }
+
+    @GetMapping("view/{skillId}")
+    public String displayViewSkill(Model model, @PathVariable int skillId) {
+
+        Optional optSkill = skillRepository.findById(skillId);
+        if (optSkill.isPresent()) {
+            Skill skill = (Skill) optSkill.get();
+            model.addAttribute("skill", skill);
+            return "skills/view";
+        } else {
+            return "redirect:../";
+        }
+    }
+
 
 }
